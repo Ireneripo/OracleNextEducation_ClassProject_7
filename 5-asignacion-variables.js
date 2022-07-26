bacc59bcbedc9f0ad68753ba9b95240645836c02
Irene Riportella <riportellairene@gmail.com>
2022-07-26 14:14:22 +0200

    trained template literals

---
+++ 5-asignacion-variables.js
@@ -0,0 +1,12 @@
+console.log('Asignando valores a las variables');
+
+const miNombre = 'Irene';
+console.log(miNombre);
+
+const miApellido = 'Riportella';
+
+const miNombreCompleto = miNombre + ' ' + miApellido;
+const miNombrePlantilla = `Mi nombre completo es ${miNombre} ${miApellido}`;
+
+console.log(miNombreCompleto);
+console.log(miNombrePlantilla);
