bac866865e52519fb1b1e392a6f747aab6b9828e
Irene Riportella <riportellairene@gmail.com>
2022-07-26 14:31:19 +0200

    training with arrays

---
+++ 6-trabajando-con-listas.js
@@ -0,0 +1,16 @@
+console.log(`Trabajando con listas`);
+const bogota = 'Bogotá';
+const lima = 'Lima';
+const santiago = 'Santiago';
+const buenosAires = 'Buenos Aires';
+
+console.log(bogota, lima, santiago, buenosAires);
+
+const ciudades = ['Bogotá', 'Lima', 'Santiago', 'Buenos Aires'];
+console.log(ciudades);
+
+const otrasCiudades = new Array();
+otrasCiudades.push('Madrid', 'Paris', 'Roma', 'Londres');
+otrasCiudades.push('Berlín');
+
+console.log(otrasCiudades);
